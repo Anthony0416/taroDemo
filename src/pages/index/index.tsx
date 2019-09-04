8576f402413ae5fe8735fc5dfea2e88a11453f47
Anthony0416 <csw0416@hotmail.com>
2019-09-04 10:26:53 +0800

    初始

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,46 @@
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
+    navigationBarTitleText: '首页'
+  }
+
+  state = {
+    positions: []
+  }
+
+  componentWillMount () { }
+
+  touchEvent = (e) => {
+    let positions = []
+    for (let i = 0; i < 10; i++) {
+      e.touches[i] ? positions.push(e.touches[i]) : ''
+    }
+    this.setState({
+      positions: positions
+    })
+  }
+
+  render () {
+    const { positions } = this.state
+    const Colors = ['#ff0000', '#ff3300', '#ff6600', '#ff9900', '#ffff00', '#99ff00', '#00ff00', '#00ffff', '#0000ff', '#6600ff']
+    return (
+      <View className='index' onTouchStart={this.touchEvent} onTouchMove={this.touchEvent}>
+        {positions.map((item, index) =>
+          <Text key={item.toString()}>{index}</Text>
+          <View style={{background: Colors[index], left: item.clientX, top: item.clientY}}></View>
+        )}
+      </View>
+    )
+  }
+}
